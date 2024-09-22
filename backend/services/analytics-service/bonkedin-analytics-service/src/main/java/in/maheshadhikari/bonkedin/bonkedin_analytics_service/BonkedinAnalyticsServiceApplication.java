b9798f43fbf9b0f1d49e8b8ec5580f9280aefcf3
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2024-09-22 08:50:56 +0100

    Initialised analytics service

---
+++ backend/services/analytics-service/bonkedin-analytics-service/src/main/java/in/maheshadhikari/bonkedin/bonkedin_analytics_service/BonkedinAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package in.maheshadhikari.bonkedin.bonkedin_analytics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonkedinAnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonkedinAnalyticsServiceApplication.class, args);
+	}
+
+}
---
+++ backend/services/analytics-service/bonkedin-analytics-service/src/test/java/in/maheshadhikari/bonkedin/bonkedin_analytics_service/BonkedinAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.maheshadhikari.bonkedin.bonkedin_analytics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonkedinAnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
